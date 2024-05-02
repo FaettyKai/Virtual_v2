4651e55360d963ea72afa4893d11905b6fd991c5
Michael <140651581+FaettyKai@users.noreply.github.com>
2024-05-02 00:44:10 -0500

    Changed layout of page 2 to slide/carousel design for ease of use.

--- script.js
+++ script.js
@@ -196,3 +196,38 @@ const youtube = document.querySelector(".youtube img");
 youtube.addEventListener("click", function(event) {
     openInNewTab("https://www.youtube.com/@outerheavenvirtual");
 });
+
+
+
+/* 4/22/2024: For format change on page 2 Slide/carousel */
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+}
+
+/* END OF CHANGES */
